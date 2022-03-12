73cde8643640e9a6c402f9dd01b64398806c7463
Jakub Bodak <bodakj@rferl.org>
2022-03-12 20:26:50 +0100

    added rivers with simple directional controls

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const terrainMap = {
     hills: "#ccff66",
     mountains: "#669999",
     lakes: "#3399ff",
-    reset: "#fff"
+    none: "#fff"
 }
 
 const a = 2 * Math.PI / 6;
@@ -25,7 +25,7 @@ function drawHexagon(x, y) {
     ctx.closePath();
     ctx.stroke();
 
-    hexes.push([x, y]);
+    hexes.push({cd: [x, y], terrain: "none", angle: null});
 }
 
 // hexagons and grid drawing from https://eperezcosano.github.io/hex-grid/
@@ -49,12 +49,35 @@ canvas.addEventListener("click", function(e) {
     colorHex(hexIdx, terrainType);
 })
 
+canvas.addEventListener('wheel', function(e) {
+    rotateRiver(e.wheelDelta > 0, e.clientX, e.clientY);
+})
+
+function rotateRiver(up, x, y) {
+    const hexIdx = findHex(x, y);
+    const hex = hexes[hexIdx];
+    
+    if (hex.terrain !== 'river') {
+        return;
+    }
+
+    let angle = hex.angle || 1;
+
+    if (up) {
+        angle++;
+    } else {
+        angle--;
+    }
+    hex.angle = angle;
+
+    renderRiver(hex)
+}
+
 function findHex(x, y) {
     const idx = hexes.findIndex(function(hex) {
-        return (x < hex[0] + 43.3 && x > hex[0] - 43.3) &&
-        (y < hex[1] + 43.3 && y > hex[1] - 43.3)
+        return (x < hex.cd[0] + 43.3 && x > hex.cd[0] - 43.3) &&
+        (y < hex.cd[1] + 43.3 && y > hex.cd[1] - 43.3)
     })
-
     return idx;
 }
 
@@ -64,14 +87,93 @@ function getTerrainType() {
 
 function colorHex(index, terrainType) {
     const hexCntr = hexes[index];
-    
-    ctx.beginPath();
+
+    if (hexCntr.terrain === terrainType) {
+        return;
+    }
+
+    if (terrainType === 'river') {
+        renderRiver(hexCntr, 0);
+    } else {
+        ctx.beginPath();
         for (var i = 0; i < 6; i++) {
-            ctx.lineTo(hexCntr[0] + r * Math.cos(a * i), hexCntr[1] + r * Math.sin(a * i));
+            ctx.lineTo(hexCntr.cd[0] + r * Math.cos(a * i), hexCntr.cd[1] + r * Math.sin(a * i));
         }
         ctx.fillStyle = terrainMap[terrainType];
         ctx.fill();
         ctx.stroke();
+        hexes[index].terrain = terrainType;
+    }
+}
+
+function renderRiver(hex, angle) {
+    ctx.beginPath();
+    for (var i = 0; i < 6; i++) {
+        ctx.lineTo(hex.cd[0] + r * Math.cos(a * i), hex.cd[1] + r * Math.sin(a * i));
+    }
+    ctx.stroke();
+    ctx.fillStyle = terrainMap['plains'];
+    ctx.fill();
+    ctx.strokeStyle = terrainMap['lakes'];
+    ctx.closePath();
+    
+    ctx.beginPath();
+    ctx.lineWidth = 4;
+    console.log(hex.angle)
+
+    // TODO: This has to be done in a different way than IF statement, because of scalability, but good enough for POC
+
+    if (hex.angle === 1) {
+        ctx.moveTo(hex.cd[0] + r, hex.cd[1]);
+        ctx.lineTo(hex.cd[0] - r, hex.cd[1]);
+    } else if (hex.angle === 2) {
+        ctx.moveTo(hex.cd[0] - Math.sin(a) * Math.sin(a) * r, hex.cd[1] - Math.cos(a) * Math.sin(a) * r)
+        ctx.lineTo(hex.cd[0] + Math.sin(a) * Math.sin(a) * r, hex.cd[1] + Math.cos(a) * Math.sin(a) * r)
+    } else if (hex.angle === 3) {
+        ctx.moveTo(hex.cd[0] + Math.cos(a) * r, hex.cd[1] + Math.sin(a) * r)
+        ctx.lineTo(hex.cd[0] - Math.cos(a) * r, hex.cd[1] - Math.sin(a) * r)
+    } else if (hex.angle === 4) {
+        ctx.moveTo(hex.cd[0], hex.cd[1] + Math.sin(a) * r)
+        ctx.lineTo(hex.cd[0], hex.cd[1] - Math.sin(a) * r)
+    } else if (hex.angle === 5) {
+        ctx.moveTo(hex.cd[0] - Math.cos(a) * r, hex.cd[1] + Math.sin(a) * r)
+        ctx.lineTo(hex.cd[0] + Math.cos(a) * r, hex.cd[1] - Math.sin(a) * r)
+    } else if (hex.angle === 6) {
+        ctx.moveTo(hex.cd[0] + Math.sin(a) * Math.sin(a) * r, hex.cd[1] - Math.cos(a) * Math.sin(a) * r)
+        ctx.lineTo(hex.cd[0] - Math.sin(a) * Math.sin(a) * r, hex.cd[1] + Math.cos(a) * Math.sin(a) * r)
+    } else if (hex.angle === 7) {
+        ctx.moveTo(hex.cd[0] + Math.sin(a) * Math.sin(a) * r, hex.cd[1] - Math.cos(a) * Math.sin(a) * r)
+        ctx.lineTo(hex.cd[0] - Math.sin(a) * Math.sin(a) * r, hex.cd[1] - Math.cos(a) * Math.sin(a) * r)
+    } else if (hex.angle === 9) {
+        ctx.moveTo(hex.cd[0] + Math.sin(a) * Math.sin(a) * r, hex.cd[1] + Math.cos(a) * Math.sin(a) * r)
+        ctx.lineTo(hex.cd[0] - Math.sin(a) * Math.sin(a) * r, hex.cd[1] + Math.cos(a) * Math.sin(a) * r)
+    } else if (hex.angle === 10) {
+        ctx.moveTo(hex.cd[0] + Math.sin(a) * Math.sin(a) * r, hex.cd[1] - Math.cos(a) * Math.sin(a) * r)
+        ctx.lineTo(hex.cd[0] + Math.sin(a) * Math.sin(a) * r, hex.cd[1] + Math.cos(a) * Math.sin(a) * r)
+    } else if (hex.angle === 11) {
+        ctx.moveTo(hex.cd[0], hex.cd[1] + Math.sin(a) * r)
+        ctx.lineTo(hex.cd[0], hex.cd[1] - Math.sin(a) * r)
+    } else if (hex.angle === 12) {
+        ctx.moveTo(hex.cd[0] - Math.sin(a) * Math.sin(a) * r, hex.cd[1] - Math.cos(a) * Math.sin(a) * r)
+        ctx.lineTo(hex.cd[0] - Math.sin(a) * Math.sin(a) * r, hex.cd[1] + Math.cos(a) * Math.sin(a) * r)
+    }
+    else {
+        ctx.moveTo(hex.cd[0] + r, hex.cd[1]);
+        ctx.lineTo(hex.cd[0] - r, hex.cd[1]);
+    }
+    ctx.stroke();
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 1;
+    hex.terrain = 'river';
 }
 
 drawGrid(canvas.width, canvas.height)
+
+
+// DEBUG
+
+const btn = document.getElementById("hexes");
+
+btn.addEventListener("click", function() {
+    console.log(hexes)
+})
